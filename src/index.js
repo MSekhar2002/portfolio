323ae3a97c32126d414fed346e273cde6c2a40e2
MSekhar2002 <125027907+MSekhar2002@users.noreply.github.com>
2024-09-29 16:43:01 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import LandingPage from './LandingPage';
+import PortfolioLandingPage from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PortfolioLandingPage />
+  </React.StrictMode>
+);
+
+
