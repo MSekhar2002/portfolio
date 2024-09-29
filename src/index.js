eacc686e840ce240eb18c6efba0e275de4f001a1
MSekhar2002 <125027907+MSekhar2002@users.noreply.github.com>
2024-09-29 17:00:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import LandingPage from './LandingPage';
 import PortfolioLandingPage from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
